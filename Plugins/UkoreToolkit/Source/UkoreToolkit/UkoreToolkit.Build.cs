2c0f35ae0198b4567a338d6fc34a6f49582fbe3b
Natchapon Srisuk <natchapon.18851@gmail.com>
2026-03-03 18:28:54 +0700

    Setup Unreal Engine Commit

---
+++ Plugins/UkoreToolkit/Source/UkoreToolkit/UkoreToolkit.Build.cs
@@ -0,0 +1,81 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using UnrealBuildTool.Rules;
+
+public class UkoreToolkit : ModuleRules
+{
+	public UkoreToolkit(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                System.IO.Path.GetFullPath(Target.RelativeEnginePath)+"/Plugins/Runtime/GeometryCache/Source/GeometryCacheTracks/Classes",
+                System.IO.Path.GetFullPath(Target.RelativeEnginePath)+"Source/Editor/Blutility/Private",
+                System.IO.Path.GetFullPath(Target.RelativeEnginePath)+"Source/Editor/Blutility/Private"
+
+            }
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "ContentBrowser",
+                "AssetRegistry",
+                "Core",
+                "Blutility",
+                "EditorScriptingUtilities",
+                "UnrealEd",
+                "InputCore",
+                "Slate",
+                "AssetRegistry",
+                "LevelSequence",
+                "MovieSceneTools",
+                "LevelSequenceEditor",
+                "DeveloperSettings",
+                "ToolMenus",
+                "MovieScene",
+                "GeometryCache",
+                "GeometryCacheTracks"
+
+				// ... add other public dependencies that you statically link with here ...
+			}
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "EditorScriptingUtilities",
+                "UnrealEd",
+                "AlembicLibrary",
+                "AlembicImporter",
+                            "MovieScene",
+                "GeometryCache",
+                "GeometryCacheTracks"
+				// ... add private dependencies that you statically link with here ...	
+			}
+            );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+				// ... add any modules that your module loads dynamically here ...
+			}
+            );
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# KafkaProj
+
+Developed with Unreal Engine 5
